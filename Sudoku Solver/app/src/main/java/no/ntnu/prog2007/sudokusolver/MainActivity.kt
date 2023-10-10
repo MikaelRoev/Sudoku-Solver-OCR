cee060c3310492c9399f76a3135fba58877153ec
Mikael <mikaelrm@stud.ntnu.no>
2023-10-10 12:39:35 +0200

    First commit, with setting up the project in Android Studio

---
+++ Sudoku Solver/app/src/main/java/no/ntnu/prog2007/sudokusolver/MainActivity.kt
@@ -0,0 +1,11 @@
+package no.ntnu.prog2007.sudokusolver
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
