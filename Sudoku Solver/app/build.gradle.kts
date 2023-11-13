071c748c6da682f31124c19e4783117ad66a5e85
odast <oksteins@stud.ntnu.no>
2023-11-13 16:26:10 +0100

    Made some small adjustments

--- Sudoku Solver/app/build.gradle.kts
+++ Sudoku Solver/app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "no.ntnu.prog2007.sudokusolver"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "no.ntnu.prog2007.sudokusolver"
         minSdk = 26
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
