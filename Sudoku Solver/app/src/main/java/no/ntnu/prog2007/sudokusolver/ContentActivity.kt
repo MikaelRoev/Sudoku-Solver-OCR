2e27e47e36d8eadf347236e06f2baaeebe6176b1
odast <oksteins@stud.ntnu.no>
2023-11-14 13:34:27 +0100

    Made initial code for the ContentActivity class

--- Sudoku Solver/app/src/main/java/no/ntnu/prog2007/sudokusolver/ContentActivity.kt
+++ Sudoku Solver/app/src/main/java/no/ntnu/prog2007/sudokusolver/ContentActivity.kt
@@ -2,10 +2,30 @@ package no.ntnu.prog2007.sudokusolver
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import no.ntnu.prog2007.sudokusolver.databinding.ActivityContentBinding
 class ContentActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityContentBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_content)
+
+        binding = ActivityContentBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.navView
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(R.id.navigation_home, R.id.navigation_insert,
+                R.id.navigation_filechooser, R.id.navigation_info)
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
     }
 }
