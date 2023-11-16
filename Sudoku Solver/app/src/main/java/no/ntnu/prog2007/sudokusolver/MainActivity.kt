8938f68184d1850a86b30590df70b1ece725847f
odast <oksteins@stud.ntnu.no>
2023-11-16 13:55:26 +0100

    Added toolbar functionality to fix bugs

--- Sudoku Solver/app/src/main/java/no/ntnu/prog2007/sudokusolver/MainActivity.kt
+++ Sudoku Solver/app/src/main/java/no/ntnu/prog2007/sudokusolver/MainActivity.kt
@@ -2,6 +2,7 @@ package no.ntnu.prog2007.sudokusolver
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -19,6 +20,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val navView: BottomNavigationView = binding.navView
+        var toolbar: Toolbar = findViewById(R.id.toolbar)
 
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
 
@@ -26,7 +28,12 @@ class MainActivity : AppCompatActivity() {
             setOf(R.id.navigation_home, R.id.navigation_insert,
                 R.id.navigation_filechooser, R.id.navigation_info)
         )
+        setSupportActionBar(toolbar)
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+
     }
+
+
 }
